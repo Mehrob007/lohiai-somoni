4c289582fc30480ee14a17cfb0d6b1fb99be14c9
Mehrob <mehrobsanginov2006@gmail.com>
2025-06-28 21:22:58 +0500

    feat: add custom hook useMediaQuery for responsive design handling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,38 +1,130 @@
+"use client";
 import navBar from "@/data/menu";
+import useMediaQuery from "@/hooks/useMediaQuery";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import { usePathname } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 export default function Header() {
+  const isMobile = useMediaQuery("(max-width: 768px)");
+  const [openBurger, setOpenBurger] = useState<boolean>(false);
+  const pathName = usePathname();
+
+  console.log("isMobile", isMobile);
+
+  useEffect(() => {
+    if (pathName) {
+      setOpenBurger(false);
+    }
+  }, [pathName]);
+
   return (
     <div className="header">
       <div className="header-banner">
         {/* <Image width={88} height={117} src={"/icons/icon-banner2.png"} alt="icon-banner" /> */}
-        <div>
-          <Image
-            width={170}
-            height={177}
-            src={"/icons/icon-banner1.png"}
-            alt="icon-banner"
-          />
-          <h1>Академияи Вазорати корҳои дохилии Ҷумҳурии Тоҷикистон </h1>
-        </div>
+
+        {isMobile ? (
+          <>
+            <Image
+              className="logo1"
+              width={170}
+              height={177}
+              src={"/icons/logo1.png"}
+              alt="icon-banner"
+            />
+            <h1>коллеҷи милитсияи вазорати корҳои дохилии тоҷикистон</h1>
+            <main>
+              {" "}
+              <Image
+                className="flag"
+                width={170}
+                height={177}
+                src={"/icons/flag-bunner.png"}
+                alt="icon-banner"
+              />
+              <Image
+                className="building"
+                width={500}
+                height={177}
+                src={"/icons/building-bunner.png"}
+                alt="icon-banner"
+              />{" "}
+              <Image
+                className="logo2"
+                width={170}
+                height={177}
+                src={"/icons/logo2.png"}
+                alt="icon-banner"
+              />
+            </main>
+          </>
+        ) : (
+          <>
+            {" "}
+            <div>
+              {" "}
+              <Image
+                width={170}
+                height={177}
+                src={"/icons/icon-banner1.png"}
+                alt="icon-banner"
+              />
+              <h1>Академияи Вазорати корҳои дохилии Ҷумҳурии Тоҷикистон </h1>
+            </div>{" "}
+          </>
+        )}
       </div>
-      <ul className="header-navbar">
-        {navBar.map((e, i) => (
-          <li key={i} className={e.class || ""}>
-            <Link href={e.url || ""}>{e.label}</Link>
-            <ol>
-              {e.subUrl &&
-                e.subUrl.map((e, i) => (
-                  <li key={i}>
-                    <Link href={e.url || ""}> {e.label}</Link>
+
+      {isMobile ? (
+        <div
+          className="header-navbar"
+          style={{ height: openBurger ? "455px" : "" }}
+        >
+          <div className="burger-menu-div">
+            <button onClick={() => setOpenBurger(!openBurger)}>
+              <GiHamburgerMenu />
+            </button>
+          </div>
+          <ol
+            className="burger-menu"
+            style={{ translate: openBurger ? "0 50px" : "0 -299px" }}
+          >
+            {navBar.map((e, i) => (
+              <>
+                {e.url && (
+                  <li key={i} className={e.class || ""}>
+                    <Link href={e.url || ""}>{e.label}</Link>
                   </li>
-                ))}
-            </ol>
-          </li>
-        ))}
-      </ul>
+                )}
+                {e.subUrl &&
+                  e.subUrl.map((e, i) => (
+                    <li key={i}>
+                      <Link href={e.url || ""}> {e.label}</Link>
+                    </li>
+                  ))}
+              </>
+            ))}
+          </ol>
+        </div>
+      ) : (
+        <ul className="header-navbar">
+          {navBar.map((e, i) => (
+            <li key={i} className={e.class || ""}>
+              <Link href={e.url || ""}>{e.label}</Link>
+              <ol>
+                {e.subUrl &&
+                  e.subUrl.map((e, i) => (
+                    <li key={i}>
+                      <Link href={e.url || ""}> {e.label}</Link>
+                    </li>
+                  ))}
+              </ol>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
---
+++ src/hooks/useMediaQuery.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { useEffect, useState } from "react";
+
+const useMediaQuery = (query: string) => {
+  const [matches, setMatches] = useState(false); // по умолчанию false
+
+  useEffect(() => {
+    if (typeof window === "undefined") return;
+
+    const media = window.matchMedia(query);
+    setMatches(media.matches); // установка начального значения
+
+    const listener = () => setMatches(media.matches);
+    media.addEventListener("change", listener);
+
+    return () => media.removeEventListener("change", listener);
+  }, [query]);
+
+  return matches;
+};
+
+export default useMediaQuery;

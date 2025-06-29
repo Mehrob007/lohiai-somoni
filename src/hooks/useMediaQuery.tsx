ad19c4e812cfdcdff30b5c1ec297dd5cbc26787a
Mehrob <mehrobsanginov2006@gmail.com>
2025-06-29 23:39:11 +0500

    fix: Clean up comments in useMediaQuery hook for clarity

--- src/hooks/useMediaQuery.tsx
+++ src/hooks/useMediaQuery.tsx
@@ -2,13 +2,13 @@
 import { useEffect, useState } from "react";
 
 const useMediaQuery = (query: string) => {
-  const [matches, setMatches] = useState(false); // по умолчанию false
+  const [matches, setMatches] = useState(false); 
 
   useEffect(() => {
     if (typeof window === "undefined") return;
 
     const media = window.matchMedia(query);
-    setMatches(media.matches); // установка начального значения
+    setMatches(media.matches); 
 
     const listener = () => setMatches(media.matches);
     media.addEventListener("change", listener);

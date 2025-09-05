9134374eed402e0b8214efa3179c553e7dc430b5
Mehrob007 <mehrobsanginov2006@gmail.com>
2025-09-05 09:41:44 +0500

    add: fonts and remuve link google fonts

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"sXMldGGpDjFbI4tdSPJT2BglC2Wg5YyCBlb+I/n89eU=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
--- .next/static/chunks/94730671-8f1b1a4cda0ccaf7.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[204],{3347:(t,r,a)=>{a.d(r,{vMW:()=>c});var e=a(4436);function c(t){return(0,e.k5)({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"currentColor"},child:[{tag:"path",attr:{d:"M10.8284 12.0007L15.7782 16.9504L14.364 18.3646L8 12.0007L14.364 5.63672L15.7782 7.05093L10.8284 12.0007Z"},child:[]}]})(t)}}}]);
